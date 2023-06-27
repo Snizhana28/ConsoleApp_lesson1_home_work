291b5424ed2d67680d2463894011d73287328756
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-06-27 21:10:12 +0300

    task 2

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter a first number : ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second number : ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            int result = firstNumber * secondNumber / 100;
+            Console.WriteLine("Result : " + result);
         }
     }
 }
