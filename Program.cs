af0f0afa4eb9387398c3f59304bde33182f26100
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-06-27 21:05:06 +0300

    task 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            
+            int number = 0;
+            Console.WriteLine("Enter number from 1 to 100");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number >= 1 && number <= 100)
+            {
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("Fizz Buzz");
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
         }
     }
 }
