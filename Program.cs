17cb2dfca47bfe03366730747fb6adc99e0e45b9
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-07-03 11:19:25 +0300

    Done task5

--- Program.cs
+++ Program.cs
@@ -2,9 +2,32 @@
 {
     internal class Program
     {
+        static string GetSeason(int month)
+        {
+            if (month >= 3 && month <= 5)
+                return "Spring";
+            else if (month >= 6 && month <= 8)
+                return "Summer";
+            else if (month >= 9 && month <= 11)
+                return "Autumn";
+            else
+                return "Winter";
+        }
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter a data (day.month.year) : ");
+            string input = Console.ReadLine();
+
+            DateTime date;
+            if (DateTime.TryParseExact(input, "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None, out date))
+            {
+                string season = GetSeason(date.Month);
+                string dayOfweek = date.ToString("dddd");
+
+                Console.WriteLine($"{season} {dayOfweek}");
+            }
+            else
+                Console.WriteLine("Invalid date format");
         }
     }
 }
