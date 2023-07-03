f2313196c03eaf891f2ce77e4f94c243ce36fd3f
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-07-03 11:30:08 +0300

    Done task7

--- Program.cs
+++ Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter the beginning of the range:");
+            int start = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the end of the range:");
+            int end = Convert.ToInt32(Console.ReadLine());
+
+            if (start > end)  // Normalise 
+            {
+                int temp = start;
+                start = end;
+                end = temp;
+            }
+
+            Console.WriteLine($"Even numbers in the range from {start} to {end}:");
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+            }
         }
     }
 }
